e47803333a21a4472254a248d16c24774bcd6e62
Leandro Carnibella <211507136+lhcarnibella@users.noreply.github.com>
2026-07-08 21:22:52 -0300

    feat: add TransfersPage and TransferConfirmationModal page objects

---
+++ pages/TransferConfirmationModal.ts
@@ -0,0 +1,14 @@
+import type { Page, Locator } from '@playwright/test';
+
+export class TransferConfirmationModal {
+    readonly page: Page;
+    readonly confirmButton: Locator;
+    readonly cancelButton: Locator;
+
+constructor (page: Page) {
+    this.page = page;
+    this.confirmButton = page.getByRole('button', { name: 'Confirmar' });
+    this.cancelButton = page.getByRole('button', { name: 'Cancelar' });
+    }
+
+}
